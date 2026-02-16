765e010cfc12693a4e219d003dd4c1f9ee1127ac
iamtomij <helloiamtomij@gmail.com>
2026-02-16 23:52:22 +0600

    Display the physics marks as output

---
+++ task3.js
@@ -0,0 +1,13 @@
+//  Display the physics marks as output
+
+const student = {
+  name : "Tomij Uddin",
+  id: 34875,
+  physics: {
+    subject: "HSC Physics",
+    author: "Haque Publication",
+    marks: 30,
+  }
+};
+
+console.log(student.physics.marks);
